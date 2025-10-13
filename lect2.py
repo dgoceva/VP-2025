d09a23fe460f5810510ebc6c26450b1aaf9b049c
dgoceva <44052125+dgoceva@users.noreply.github.com>
2025-10-13 13:16:19 +0300

    13/10/2025

--- lect2.py
+++ lect2.py
@@ -1,6 +1,7 @@
 
 import random
 
+# list mutable
 l = [1,2,3,4,5]
 print(l)
 
--- lect3.py
+++ lect3.py
@@ -1,5 +1,8 @@
 
 import random
+from collections import deque
+# pip install numpy --upgrade
+import numpy as np
 
 l = []
 for i in range(10):
@@ -31,8 +34,8 @@ if len(ll) > 0:
 else:
     print("No such data...")
 
-l1 = [int(input(f"{i}=")) for i in range(3)]
-print(l1)
+# l1 = [int(input(f"{i}=")) for i in range(3)]
+# print(l1)
 
 # queue
 # First In First Out FIFO
@@ -47,3 +50,76 @@ l.append(444)
 print(l)
 print("last:",l.pop())
 print(l)
+
+# deque
+dq = deque(l)
+print(dq)
+
+# FIFO
+dq.append(-111)
+print(dq)
+dq.popleft()
+print(dq)
+
+# LIFO
+dq.append(-222)
+print(dq)
+dq.pop()
+print(dq)
+
+# tuple immutable
+t = ()
+print(type(t),t)
+t = (1,2,3,4,5)
+print(t)
+x1,x2,x3,x4,x5 = t
+print(x1,x2,x3,x4,x5)
+t1 = x1,x2,x3,x4
+print(type(t1),t1)
+t = (1,)
+print(type(t),t)
+t = 1
+print(type(t),t)
+t = (1)
+print(type(t),t)
+
+t = (1,2,3,4)
+print(t[1])
+# t[1] = 11
+
+t = tuple(l)
+print(type(l),l)
+print(type(t),t)
+
+print("count:",t.count(20))
+if -1 in t:
+    print(t.index(-1))
+
+l11 = list(t)
+print(l11)
+
+# matrix
+m = [[1,2,3],[4,5,6]]
+print(type(m),m)
+for row in m:
+    for x in row:
+        print(x, end=" ")
+    print()
+
+m = [[random.randint(0,100) for col in range(4)] 
+     for row in range(3)]
+print(m)
+
+m = []
+for row in range(3):
+    l = []
+    for col in range(4):
+        l.append(random.randint(0,10))
+    m.append(l)
+print(m)
+
+# m = np.matrix([[1,2],[4,5]])
+# print(type(m),m)
+
+l2 = [x for x in np.arange(0,10,2.5)]
+print(type(l2),l2)
